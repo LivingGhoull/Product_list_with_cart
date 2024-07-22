c5fe5caabecdc69cc92055797b931a1d589144fa
Jesper Kjærgaard <jezper@hotmail.dk>
2024-07-22 10:14:53 +0200

    feat: add product for mobile
    design of product have gotton the needed styling and is made for mobile for now

--- src/App.tsx
+++ src/App.tsx
@@ -7,41 +7,42 @@ import productList from "./utility/productList.json";
 import WindowWidth from "./hooks/WindowWidth";
 
 function App() {
-  const getWindowsWidth = WindowWidth()
+  const getWindowsWidth = WindowWidth();
   const [screenType, setScreenType] = useState("mobile");
-
 
   useEffect(() => {
     if (getWindowsWidth >= 1440) {
-      setScreenType("desktop")
-    } else if(getWindowsWidth >= 786) {
-      setScreenType("tablet")
+      setScreenType("desktop");
+    } else if (getWindowsWidth >= 786) {
+      setScreenType("tablet");
     } else {
-      setScreenType("mobile")
+      setScreenType("mobile");
     }
-  }, [getWindowsWidth])
+  }, [getWindowsWidth]);
 
   return (
-    <main>
-      <h1>Desserts</h1>
+    <main className="container">
+      <h1 className="title">Desserts</h1>
 
-      {productList.deserts.map((product, index) => (
-        <div key={index}>
-          <Product
-            src={
-              screenType == "desktop"
-                ? product.images.desktop
-                : screenType == "tablet"
-                ? product.images.desktop
-                : product.images.mobile
-            }
-            alt={product.alt}
-            type={product.type}
-            title={product.title}
-            price={product.price}
-          />
-        </div>
-      ))}
+      <div className="products">
+        {productList.deserts.map((product, index) => (
+          <div key={index}>
+            <Product
+              src={
+                screenType == "desktop"
+                  ? product.images.desktop
+                  : screenType == "tablet"
+                  ? product.images.desktop
+                  : product.images.mobile
+              }
+              alt={product.alt}
+              type={product.type}
+              title={product.title}
+              price={product.price}
+            />
+          </div>
+        ))}
+      </div>
 
       <Cart />
     </main>
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,18 +2,22 @@ interface Props {
   title: string;
   type: string;
   price: string;
-  
+
   src: string;
   alt: string;
 }
 
 function Product(prop: Props) {
-  return <>
-    <img src={prop.src} alt={prop.alt} />
-    <p>{prop.type}</p>
-    <p>{prop.title}</p>
-    <p>{prop.price}</p>  
-  </>;
+  return (
+    <div className="products__card">
+      <img src={prop.src} alt={prop.alt} />
+      <div>
+        <p className="products__type">{prop.type}</p>
+        <h6 className="products__title">{prop.title}</h6>
+        <p className="products__price">{prop.price}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
